20a5b338ae1209668432667d171c9e196024a405
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2024-03-18 15:10:08 +0100

    Add L1 regularization to ProdLDA

--- src/models/losses.py
+++ src/models/losses.py
@@ -13,6 +13,16 @@ def l2_penalty(parameters) -> float:
     """
     return torch.linalg.norm(parameters, ord="fro")**2
 
+def l1_penalty(parameters) -> float:
+    """Calculate the L2 penalty for the given parameters.
+
+    Args:
+        parameters (iterable): An iterable of parameters.
+
+    Returns:
+        float: The L2 penalty.
+    """
+    return torch.linalg.norm(parameters, ord=1)
 
 def cosine_penalty(parameters) -> float:
     """Calculate the cosine penalty for the given parameters.
@@ -23,4 +33,4 @@ def cosine_penalty(parameters) -> float:
     Returns:
         float: The cosine penalty.
     """
-    return 1. - torchmetrics.functional.pairwise_cosine_similarity(parameters, reduction="sum").sum()
+    return torchmetrics.functional.pairwise_cosine_similarity(parameters, reduction="sum").sum()
